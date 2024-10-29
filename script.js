215861a5531bd7d58c5cd7784ce1f21dbe931bdc
Homey Lau <lhw1953927853@gmail.com>
2024-10-29 12:02:30 +0800

    update script structure

--- script.js
+++ script.js
@@ -1,51 +1,68 @@
-const createPlayer = (myId, myMark) => {
-  const id = myId;
-  const mark = myMark;
-  return { id, mark };
-};
-
 const GameBoard = (() => {
+  const createPlayer = (myId, myMark) => {
+    const id = myId;
+    const mark = myMark;
+    return { id, mark };
+  };
   const players = [createPlayer(1, "X"), createPlayer(2, "O")];
+
   let blocks = [];
   let currentPlayerNumber = 0;
-
-  const togglePlayer = () => {
-    currentPlayerNumber = (currentPlayerNumber + 1) % 2;
-  };
+  const lines = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
 
   const calculateWinner = () => {
-    let lines = [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
-      [1, 4, 7],
-      [2, 5, 8],
-      [3, 6, 9],
-      [1, 5, 9],
-      [3, 5, 7],
-    ];
     for (let line of lines) {
+      let [a, b, c] = line;
       if (
-        blocks[line[0] - 1] != null &&
-        blocks[line[1] - 1] != null &&
-        blocks[line[2] - 1] != null &&
-        blocks[line[0] - 1] === blocks[line[1] - 1] &&
-        blocks[line[1] - 1] === blocks[line[2] - 1] &&
-        blocks[line[0] - 1] === blocks[line[2] - 1]
+        blocks[a - 1] != null &&
+        blocks[b - 1] != null &&
+        blocks[c - 1] != null &&
+        blocks[a - 1] === blocks[b - 1] &&
+        blocks[b - 1] === blocks[c - 1] &&
+        blocks[a - 1] === blocks[c - 1]
       ) {
-        return blocks[line[0] - 1];
+        return blocks[a - 1];
       }
     }
     return 0;
   };
 
-  const gameOver = (winner) => {
-    if (winner == 1) {
-      console.log("X winner!");
-    } else {
-      console.log("O winner!");
+  const togglePlayer = () => {
+    currentPlayerNumber = (currentPlayerNumber + 1) % players.length;
+  };
+
+  const markBlock = (block) => {
+    const markDiv = document.createElement("div");
+    markDiv.classList.add("mark");
+    markDiv.textContent = players[currentPlayerNumber].mark;
+    block.appendChild(markDiv);
+    console.log(
+      `block ${block.id} marked with "${players[currentPlayerNumber].mark}"`
+    );
+  };
+
+  const gameOver = (result, isEnd) => {
+    // game not over
+    if (!isEnd && result == 0) {
+      return;
     }
 
+    // someone win
+    if (result) {
+      console.log(`${players[result - 1].mark} win!`);
+    } else if (isEnd) {
+      // it's a draw!
+      console.log("Draw!");
+    }
   };
 
   const start = () => {
@@ -54,17 +71,20 @@ const GameBoard = (() => {
       item.addEventListener("click", (e) => {
         e.preventDefault();
         const block = e.target;
-        if (blocks[e.target.id - 1] == null) {
-          blocks[e.target.id - 1] = players[currentPlayerNumber].id;
-          const markDiv = document.createElement("div");
-          markDiv.classList.add("mark");
-          markDiv.textContent = players[currentPlayerNumber].mark;
-          block.appendChild(markDiv);
+        if (
+          block.classList.contains("block") &&
+          blocks[block.id - 1] === undefined
+        ) {
+          blocks[block.id - 1] = players[currentPlayerNumber].id;
+          markBlock(block);
           togglePlayer();
-          let result = calculateWinner();
-          if (result !== 0) {
-            gameOver(result);
-          }
+          let isEnd =
+            blocks.filter((item) => {
+              return item !== undefined;
+            }).length === 9;
+          gameOver(calculateWinner(), isEnd);
+        } else {
+          console.log("The Current Block is Occupied!");
         }
       });
     });
